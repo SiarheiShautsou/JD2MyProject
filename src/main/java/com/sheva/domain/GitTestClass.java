1625470632e92cd93a951dc33e18f440e721c19a
Siarhei <sheva-29@yandex,ru>
2022-07-24 17:55:57 +0300

    Cherry-pick test

--- src/main/java/com/sheva/domain/GitTestClass.java
+++ src/main/java/com/sheva/domain/GitTestClass.java
@@ -3,5 +3,6 @@ package com.sheva.domain;
 public class GitTestClass {
     public static void main(String[] args) {
         System.out.println("GitTest");
+        System.out.println("Commit for cherry-pick");
     }
 }
