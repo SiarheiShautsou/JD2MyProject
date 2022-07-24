b3a3f96c0b567718f3b89a0f2f2173f54be29a02
Siarhei <sheva-29@yandex,ru>
2022-07-24 18:23:56 +0300

    Cherry-pick and revert

--- src/main/java/com/sheva/Main.java
+++ src/main/java/com/sheva/Main.java
@@ -3,5 +3,6 @@ package com.sheva;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello new project");
+        System.out.println("Cherry-pick done");
     }
 }
